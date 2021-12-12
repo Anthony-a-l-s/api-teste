6e4e5019b4b0fb1e1b59f7d09b57a79be7fb90ca
Anthony-a-l-s <alima.als37@gmail.com>
2021-12-12 11:59:41 -0300

    'mod'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+require('dotenv').config()
 
 app.use(express.json())
 app.use(cors())
 
- app.listen (3000 ,() => {
-    console.log('Server running on port: ' + 3000)
+const port = process.env.URL || 3000
+
+ app.listen (port ,() => {
+    console.log('Server running on port: ' + port)
 })
 
 app.get('/hello_world', (req, res) => {
